3fcc29a10122c48fde3562e1a1bee130d492e295
Ayden Aba <ayden@codefishstudio.com>
2020-04-09 21:22:58 +0930

    sandboxing parsing of a draw.io xml into pages and stacks

--- src/rn_navigation_generator/sandbox.clj
+++ src/rn_navigation_generator/sandbox.clj
@@ -1,7 +1,36 @@
-;(ns rn-navigation-generator.sandbox
-;  (:require [selmer.parser :refer [render-file render] :as selmer]
-;            [selmer.util :refer [without-escaping]]
-;            [rn-navigation-generator.file-writers :as file-writers]
-;            [rn-navigation-generator.stringifiers :as s]
-;            [rn-navigation-generator.node :as node]))
-;
+(ns rn-navigation-generator.sandbox
+  (:require [clojure.xml :as xml]
+            [rn-navigation-generator.node :as node]))
+
+(def data (xml/parse "resources/sample_file.xml"))
+
+(defn extract-cells [m]
+  (if-not (or (= (:tag m) :mxCell)
+              (= (:tag m) :object))
+    (->> (:content m)
+         (map extract-cells)
+         flatten)
+    m))
+
+(defn- node-type [{{:keys [node_type]} :attrs}] node_type)
+
+(defn cells->pages [cells]
+  (let [page? #(= (node-type %) "page")
+        cell->page (fn [{{page-name :label} :attrs}]
+                     (node/map->Page {:page-name page-name}))]
+
+    (->> cells
+         (filter page?)
+         (map cell->page))))
+
+(defn cells->stacks [cells]
+  (let [stack? #(= (node-type %) "stack")
+        cell->stack (fn [{{stack-name :label} :attrs}]
+                      (node/map->Stack {:stack-name stack-name}))]
+
+    (->> cells
+         (filter stack?)
+         (map cell->stack))))
+
+(cells->pages (extract-cells data))
+(cells->stacks (extract-cells data))
