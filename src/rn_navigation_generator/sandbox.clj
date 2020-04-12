4c5d1522283b798b0b3db97488037fdbdf59c139
Ayden Aba <ayden@codefishstudio.com>
2020-04-12 21:32:40 +0930

    cleanup sandbox file + implementation of route-names stacks and pages

--- src/rn_navigation_generator/sandbox.clj
+++ src/rn_navigation_generator/sandbox.clj
@@ -2,33 +2,31 @@
   (:require [clojure.xml :as xml]
             [rn-navigation-generator.node :as node]))
 
-(def data (xml/parse "resources/sample_file.xml"))
+(def xml-data (xml/parse "resources/sample_file.xml"))
+
+(defn cell? [m] (or (= (:tag m) :mxCell)
+                    (= (:tag m) :object)))
+
+(defn- node-type [{{:keys [node_type]} :attrs}] node_type)
+(defn- page? [cell] (= (node-type cell) "page"))
+(defn- stack? [cell] (= (node-type cell) "stack"))
+
+(defn- vertex? [cell] (or (stack? cell) (page? cell)))
+(defn- vertex-id [{{:keys [id]} :attrs}] id)
+(defn- vertex-label [{{:keys [label]} :attrs}] label)
+
+(defn- edge? [{{:keys [edge]} :attrs}] (= edge "1"))
+(defn- edge-source-id [edge]
+  (let [{{source :source} :attrs} edge] source))
+(defn- edge-target-id [edge]
+  (let [{{target :target} :attrs} edge] target))
 
 (defn- extract-cells [m]
-  (if-not (or (= (:tag m) :mxCell)
-              (= (:tag m) :object))
+  (if-not (cell? m)
     (->> (:content m)
          (map extract-cells)
          flatten)
     m))
-
-(defn- node-type [{{:keys [node_type]} :attrs}] node_type)
-
-(defn- page? [cell] (= (node-type cell) "page"))
-
-(defn- stack? [cell] (= (node-type cell) "stack"))
-
-(defn- vertex? [cell] (or (stack? cell) (page? cell)))
-
-(defn- edge? [{{:keys [edge]} :attrs}] (= edge "1"))
-
-(defn- vertex-id [{{:keys [id]} :attrs}] id)
-
-(defn- edge-source-id [edge]
-  (let [{{source :source} :attrs} edge] source))
-
-(defn- edge-target-id [edge]
-  (let [{{target :target} :attrs} edge] target))
 
 (defn- extract-vertices [xml-data]
   (let [cells (extract-cells xml-data)]
@@ -44,27 +42,34 @@
   (let [edges (get grouped-edges id)]
     (assoc vertex :edges edges)))
 
-(defn- group-by-vertex-id [vertices]
-  (into {} (map #(vector (vertex-id %) %) vertices)))
-
-(defn make-graph [data]
-  (let [vertices (extract-vertices data)
-        grouped-edges (group-by edge-source-id (extract-edges data))]
-    (->> vertices
-         (map #(assoc-edges grouped-edges %))
-         group-by-vertex-id)))
-
-(defn- get-vertex [graph id] (get graph id))
-
+(defn- get-vertex [graph id] (first (filter #(= (vertex-id %) id) graph)))
 (defn- assoc-adjacent-vertices [graph vertex]
   (let [adjacent-vertices
         (->> (:edges vertex)
              (map #(get-vertex graph (edge-target-id %))))]
     (assoc vertex :adjacent-vertices adjacent-vertices)))
 
-(defn dereference-vertices [graph]
+(defn- make-graph [data]
+  (let [vertices (extract-vertices data)
+        grouped-edges (group-by edge-source-id (extract-edges data))]
+    (->> vertices
+         (map #(assoc-edges grouped-edges %)))))
+
+(defn- dereference-vertices [graph]
   "Dereferences the edges attached to each vertex by 1 layer."
   (->> graph
-       (map (fn [[_ vertex]] (assoc-adjacent-vertices graph vertex)))
-       group-by-vertex-id))
+       (map (fn [vertex] (assoc-adjacent-vertices graph vertex)))))
 
+(defn make-nav-graph [xml-data] (dereference-vertices (make-graph xml-data)))
+
+(defn route-names [nav-graph]
+  (->> nav-graph
+       (map vertex-label)))
+
+(defn stacks [nav-graph]
+  (->> nav-graph
+       (filter stack?)))
+
+(defn pages [nav-graph]
+  (->> nav-graph
+       (filter page?)))
